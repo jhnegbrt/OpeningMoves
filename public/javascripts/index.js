ea3c47cded8556d1635f2786bbf09b722a4c32bc
jhnegbrt <jhnegbrt@gmail.com>
2021-04-26 17:31:25 -0700

    adds first barchart

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -22,5 +22,33 @@ document.addEventListener('DOMContentLoaded', () => {
     p.appendChild(text)
     let element = document.getElementById("main")
     element.appendChild(p)
+
+    let svgWidth = 500;
+    let svgHeight = 300;
+
+    let svg = d3.select('svg')
+      .attr("width", svgWidth)
+      .attr("height", svgHeight)
+      .attr("class", "bar-chart")
+    
+    let dataset = [80, 100, 120, 180]
+    let barPadding = 5
+    let barWidth = (svgWidth / dataset.length)
+
+    let barChart = svg.selectAll('rect')
+      .data(dataset)
+      .enter()
+      .append("rect")
+      .attr("y", function(d) {
+        return svgHeight - d
+      })
+      .attr("height", function(d){
+        return d
+      })
+      .attr("width", barWidth - barPadding)  
+      .attr("transform", function (d, i) {  
+           var translate = [barWidth * i, 0];  
+           return "translate("+ translate +")";  
+      });
     
 })
