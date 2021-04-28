60c2a67a84de15f31217ebf4680665c7899389b0
jhnegbrt <jhnegbrt@gmail.com>
2021-04-28 14:49:06 -0700

    creates d3 chart with data from backend

--- public/javascripts/chart_util.js
+++ public/javascripts/chart_util.js
@@ -1,7 +1,23 @@
-export function getChartMax(){
+export function getChartMax(chartArray){
 
+  let maxPrice = 0
+  for (let i = 0; i < chartArray.length; i++){
+    if(chartArray[i]["h"] > maxPrice){
+      maxPrice = chartArray[i]["h"]
+    }
+  }
+
+  return Math.floor(Math.floor(maxPrice) * 1.1)
 }
 
-export function getChartMin(){
-  
+export function getChartMin(chartArray){
+
+  let minPrice;
+  for (let i = 0; i < chartArray.length; i++){
+    if(minPrice === undefined || chartArray[i]["l"] < minPrice){
+      minPrice = chartArray[i]["l"]
+    }
+  }
+  return Math.floor(Math.floor(minPrice) * .8)
+
 }
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,7 @@
 import retrieveData from './data_retriever'
 import {convertData, filterData} from './data_handler'
 import createCharts from './create_charts'
+import { getChartMax, getChartMin } from './chart_util'
 
 document.addEventListener('DOMContentLoaded', loadPage)
 
@@ -15,14 +16,27 @@ async function loadPage(){
   let charts = createCharts(selectedCandles, allCandles)
 
   let chart = []
-  let chart1 = charts[0]
+  let chart1 = charts[7]
   let chartArray = []
   for(const candle in chart1){
     chartArray.push(chart1[candle])
   }
-  console.log(chartArray)
 
-  
+  let chartMax = getChartMax(chartArray)
+  let chartMin = getChartMin(chartArray)
+
+  console.log(chartMax, chartMin)
+
+  const xScale = d3
+    .scaleBand()
+    .domain(chartArray.map((dataPoint) => dataPoint.t))
+    .rangeRound([0, 500])
+    .padding(0.1)
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, chartMax])
+    .range([300, 0]);
 
   const container = d3.select('#second')
     .style('border', '1px solid blue')
@@ -31,6 +45,16 @@ async function loadPage(){
   const candles = container
     .selectAll('.candle')
     .data(chartArray)
+    .enter()
+    .append('rect')
+    .classed('candle', true)
+    .style('fill', 'red')
+    .attr('width', xScale.bandwidth())
+    .attr('height', data => yScale(data.h - data.l))
+    .attr('x', data => xScale(data.t))
+    // .attr('y', data => yScale(data.h - data.l))
+    .attr('y', 0)
+
 
 
   let svgWidth = 250;
