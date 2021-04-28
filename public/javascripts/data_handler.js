aac481caa87026db7d9f7a6c4633ba01670a4115
jhnegbrt <jhnegbrt@gmail.com>
2021-04-27 17:49:38 -0700

    adds functions to sort out volatile and opening candles

--- public/javascripts/data_handler.js
+++ public/javascripts/data_handler.js
@@ -1,10 +1,7 @@
 export function convertData(data){
-
   let candles = {};
-
   //put each candle together
   for(let i = 0; i < data["c"].length; i++){
-
     candles[data["t"][i]] = {
       c: data["c"][i],
       h: data["h"][i],
@@ -14,36 +11,49 @@ export function convertData(data){
       v: data["v"][i]
     }
   }
-
   return candles
-
 }
 
 export function filterData(candles){
 
   let openingCandles = selectOpeningCandles(candles)
-  debugger
+  console.log(openingCandles)
   let filteredCandles = selectVolatileCandles(openingCandles)
+  console.log(filteredCandles)
   return filteredCandles
 
 }
 
+
 function selectOpeningCandles(candles){
-
   let openingCandles = {}
-
   for(const candle in candles){
     var date = new Date(candle * 1000)
     var convertedTime = date.toLocaleTimeString("en-US", {timeZone: "America/New_York"})
-    console.log(date)
     if (convertedTime === "9:30:00 AM"){
-      
       openingCandles[candle] = candles[candle]
     }
   }
   return openingCandles
 }
 
+function volatileCandle(candle){
+  if((candle["o"] - candle["l"]) / candle["o"] >= .01 || (candle["h"] - candle["o"]) / candle["o"] >= .01){
+    return true
+  } else{
+    return false
+  }
+  
+}
+
 function selectVolatileCandles(openingCandles){
 
+  let filteredCandles = {}
+  for (const candle in openingCandles){
+    if(volatileCandle(openingCandles[candle])){
+      filteredCandles[candle] = openingCandles[candle]
+    }
+  }
+  return filteredCandles
 }
+
