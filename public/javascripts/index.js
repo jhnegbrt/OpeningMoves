23f7fc8a6a6ccd7137d933ec021f9a95dd6f1d24
jhnegbrt <jhnegbrt@gmail.com>
2021-04-29 10:40:24 -0700

    adds form handler skeleton

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,31 +1,9 @@
-import retrieveData from './data_retriever'
-import {convertData, filterData} from './data_handler'
-import generateChartData from './generate_chart_data'
-import createChart from './create_chart'
-import createTabs from './components/create_tabs'
-import { getChartMax, getChartMin } from './chart_util'
+import submitForm from './submit_form'
 
 document.addEventListener('DOMContentLoaded', loadPage)
 
-async function loadPage(){
-  let data;
-  data = await retrieveData()
-  let allCandles = convertData(data)
-
-  let selectedCandles = filterData(allCandles)
-
-  let charts = generateChartData(selectedCandles, allCandles)
-
-  createTabs(charts)
-
-  
-  let chartDataArray = []
-  let chart = charts[0]
-  for (const candle in chart){
-    chartDataArray.push(chart[candle])
-  }
-
-  createChart(chartDataArray)
-  
-
+function loadPage(){
+  let form = document.querySelector("form")
+  form.onsubmit = submitForm(form)
 }
+
---
+++ public/javascripts/submit_form.js
@@ -0,0 +1,35 @@
+import retrieveData from './data_retriever'
+import {convertData, filterData} from './data_handler'
+import generateChartData from './generate_chart_data'
+import createChart from './create_chart'
+import createTabs from './components/create_tabs'
+
+
+export default async function submitForm(form){
+
+  let ticker = form.target.ticker.value
+  let percentChange = form.target.percentChange.value
+  let dataRange = form.target.dataRange.value
+  let timeFrame = form.target.timeFrame.value
+
+  let data;
+  data = await retrieveData()
+  let allCandles = convertData(data)
+
+  let selectedCandles = filterData(allCandles)
+
+  let charts = generateChartData(selectedCandles, allCandles)
+
+  createTabs(charts)
+
+  
+  let chartDataArray = []
+  let chart = charts[0]
+  for (const candle in chart){
+    chartDataArray.push(chart[candle])
+  }
+
+  createChart(chartDataArray)
+  
+
+}
