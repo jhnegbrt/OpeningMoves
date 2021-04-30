9eb7a9c5e574021e350395d71e59733169a454bb
jhnegbrt <jhnegbrt@gmail.com>
2021-04-29 18:36:16 -0700

    stasching changes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,32 +9,3 @@ function loadPage(){
 
 
 
-
-// let seconds;
-// if (timeFrame === "3m"){
-//   seonds = 7890000
-// } else if (timeFrame === "6m"){
-//   seconds = 15780000
-// } else if (timeFrame === "1y"){
-//   seconds = 31536000
-// } else if (timeFrame === "5y"){
-//   seconds = 157680000
-// }
-// let symbol = ticker
-// let resolution = "5"
-// let to = String(Math.floor(Date.now() / 1000))
-// let from = String(Math.floor(Date.now() / 1000)-seconds)
-// let token = "c23h2raad3ieeb1lcqf0"
-
-let query = `symbol=${symbol}&resolution=${resolution}&from=${from}&to=${to}&token=${token}`
-let response = await axios.get(`/search?${query}`)
-.then((response) => {
-  return(response.data)
-})
-.catch(error => {
-  return(error) 
-})
-
-return response
-
-
