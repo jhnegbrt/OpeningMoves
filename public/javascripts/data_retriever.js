ef4b0b27435a726a1241f874d5c897fac5280991
jhnegbrt <jhnegbrt@gmail.com>
2021-04-29 21:08:15 -0700

    adds 3month query ability

--- public/javascripts/data_retriever.js
+++ public/javascripts/data_retriever.js
@@ -4,7 +4,6 @@ async function query(queryString, from, to){
 
   let token = "c23h2raad3ieeb1lcqf0"
   let query = queryString.concat(`&from=${from}&to=${to}&token=${token}`)
-  debugger
   let response = await axios.get(`/search?${query}`)
   .then((response) => {
     return(response.data)
@@ -26,13 +25,19 @@ export default async function retrieveData(ticker, dataRange) {
     if (dataRange === "3m"){
       let results = {}
       for(let i = 0; i < 3; i++){
-        let queryResult = query(queryString, from, to)
+        let queryResult = await query(queryString, from, to)
+        debugger
         for (const key in queryResult){
-          results[key] = results[key].concat(queryResult[key])
+          if (i === 0){
+            results[key] = queryResult[key]
+          } else {
+            results[key] = results[key].concat(queryResult[key])
+          }
         }
         to = String(parseInt(to) - 2505600)
         from = String(parseInt(from)) - 2505600
       }
+      return results
     } else if (dataRange === "6m"){
 
 
@@ -51,7 +56,7 @@ export default async function retrieveData(ticker, dataRange) {
     }
 
     
-
+  
 
 
 }
