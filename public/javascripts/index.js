db463c06184e0f27f1c3fecc5766660916c4bcea
jhnegbrt <jhnegbrt@gmail.com>
2021-04-29 22:12:21 -0700

    styles modal, done for the night

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,12 @@
 import submitForm from './submit_form'
+import {renderLoadingModal} from './components/loading_modal'  
 
 document.addEventListener('DOMContentLoaded', loadPage)
 
 function loadPage(){
   let form = document.querySelector("form")
   form.onsubmit = submitForm
+  renderLoadingModal()
 }
 
 
