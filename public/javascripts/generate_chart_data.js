4c761874c45c4777a805c30d82bcdf576e324fd7
jhnegbrt <jhnegbrt@gmail.com>
2021-06-02 13:16:51 -0700

    adds fix for missing candles

--- public/javascripts/generate_chart_data.js
+++ public/javascripts/generate_chart_data.js
@@ -8,7 +8,26 @@ export default function generateChartData(selectedCandles, allCandles){
       let candleString = j.toString()
       if (allCandles[candleString] !== undefined){
         chart[candleString] = allCandles[candleString]
-      } 
+      } else {
+        let priorCandle = parseInt(candleString)-300;
+        let followingCandle = parseInt(candleString) + 300;
+        while (allCandles[priorCandle] === undefined){
+          priorCandle -= 300
+        }
+        while (allCandles[followingCandle] === undefined){
+          followingCandle += 300
+        }
+        priorCandle = allCandles[priorCandle]
+        followingCandle = allCandles[followingCandle]
+        let fillCandle = {
+          c: ((priorCandle.c + followingCandle.c) / 2),
+          h: ((priorCandle.h + followingCandle.h) / 2),
+          l: ((priorCandle.l + followingCandle.l) / 2),
+          o: ((priorCandle.o + followingCandle.o) / 2),
+          t: j
+        }
+        chart[candleString] = fillCandle
+      }
     }
     charts.push(chart)
   }
