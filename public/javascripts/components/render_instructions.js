d72075c0054f17b02ff4c1bab325e0410d8b3272
jhnegbrt <jhnegbrt@gmail.com>
2021-06-01 22:10:15 -0700

    renders instructions on entry

--- public/javascripts/components/render_instructions.js
+++ public/javascripts/components/render_instructions.js
@@ -2,7 +2,13 @@ import {removeCharts, removeOverview, removeInstructions} from "../util/remove"
 
 export default function renderInstructions(){
 
-  let directionsList = []
+  let directionsList = [
+    "Input the Ticker representation of a stock or index you are interested in exploring: e.g., TSLA (Tesla Inc.).",
+    "Then, enter a minimum percent-change you are interested in and a timeframe across which you believe this percent change may appear.",
+    "For example, if you are interested in all of the days that a certain stock moved more than 1.4% within the first 10 minutes, enter '1.4' for % change, and select 10 minutes.",
+    "Finally, enter the date range you are interested in retreiving data from.",
+    "OpeningMoves will then display the daily-chart for each day that meets your selection criterion."
+  ]
 
 
   removeCharts()
@@ -18,15 +24,15 @@ export default function renderInstructions(){
   let chartContainer = document.getElementById("chart-container")
   let list = document.createElement("ol")
   list.classList.add("instructions")
+  debugger
+  directionsList.forEach(item =>{
+    let text = document.createTextNode(item)
+    let listItem = document.createElement("li")
+    listItem.appendChild(text)
+    list.appendChild(listItem)
+  })
+  instructionsContainer.appendChild(list)
   chartContainer.appendChild(instructionsContainer)
 
 }
 
-
-<li>Input the Ticker representation of a stock or index you are interested in exploring: e.g., TSLA (Tesla Inc.).</li>
-<li>Then, enter a minimum percent-change you are interested in and a timeframe across which you believe this 
-  percent change may appear.</li>
-<li>For example, if you are interested in all of the days that a certain stock moved more than 1.4% within the first
-   10 minutes, enter '1.4' for % change, and select 10 minutes.</li>
-<li>Finally, enter the date range you are interested in retreiving data from.</li>
-<li>OpeningMoves will then display the daily-chart for each day that meets your selection criterion.</li>
