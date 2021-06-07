e7445c25a2ae50fbfa5508dee15d6fc2b8a9b9ea
jhnegbrt <jhnegbrt@gmail.com>
2021-06-06 20:15:59 -0700

    creates alpha vantage query string in queryhelper and retrieve data function

--- public/javascripts/data_retriever.js
+++ public/javascripts/data_retriever.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
 
-async function query(queryString, from, to){
+async function query(queryString){
 
   // let token = "c23h2raad3ieeb1lcqf0"
   // let query = queryString.concat(`&from=${from}&to=${to}&token=${token}`)
   // let response = await axios.get(`/search?${query}`)
 
+  let query = 
 
   let response = await axios.get(`/alphaVantage`)
   .then((response) => {
@@ -17,11 +18,23 @@ async function query(queryString, from, to){
   return response
 }
 
-async function queryHelper(queryString, from, to, times){
+async function queryHelper(queryString, times){
+
+  let apiKey = "&apikey=53VI4OM5S9T1PSSI"
   let results = {}
   for(let i = 0; i < times; i++){
-    let queryResult = await query(queryString, from, to, )
+    let month = i + 1
+    let year = 1
+    if (i === 13){
+      month = 1
+      year = 2
+    }
+    let slice = `&slice=year${year}month${month}`
+    let fullString = queryString.concat(slice).concat(apiKey)
+
     debugger
+    let queryResult = await query(fullString)
+    
     for (const key in queryResult){
       if (i === 0){
         results[key] = queryResult[key]
@@ -29,8 +42,8 @@ async function queryHelper(queryString, from, to, times){
         results[key] = results[key].concat(queryResult[key])
       }
     }
-    to = String(parseInt(to) - 2505600)
-    from = String(parseInt(from)) - 2505600
+    // to = String(parseInt(to) - 2505600)
+    // from = String(parseInt(from)) - 2505600
   }
   return results
 }
@@ -38,17 +51,17 @@ async function queryHelper(queryString, from, to, times){
 export default async function retrieveData(ticker, dataRange) {
     
     let symbol = ticker
-    let resolution = "5"
-    let to = String(Math.floor(Date.now() / 1000))
-    let from = String(Math.floor(Date.now() / 1000) - 2505600)
-    let queryString = `symbol=${symbol}&resolution=${resolution}`
+    let interval = "5min"
+    // let to = String(Math.floor(Date.now() / 1000))
+    // let from = String(Math.floor(Date.now() / 1000) - 2505600)
+    let queryString = `&symbol=${symbol}&interval=${interval}`
 
     if (dataRange === "3m"){
-      return queryHelper(queryString, from, to, 3)
+      return queryHelper(queryString, 3)
     } else if (dataRange === "6m"){
-      return queryHelper(queryString, from, to, 6)
+      return queryHelper(queryString, 6)
     } else if (dataRange === "1y"){
-      return queryHelper(queryString, from, to, 12)
+      return queryHelper(queryString, 12)
     }    
   
 }
